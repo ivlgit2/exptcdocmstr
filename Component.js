e1a80b24625f5e954f1d04439a95cd2d4f09168e
ivlgit2 <ivlgit2@gmail.com>
2024-02-14 12:02:07 +0000

    initial

---
+++ Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("bri.exp_tc_doc_mstr.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
